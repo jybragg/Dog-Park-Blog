65b17d972e80e913de654493f6094eee29e894ac
Jasmin <jasymatos@gmail.com>
2019-10-26 23:02:45 -0400

    added nav and header components

--- dogpark/src/App.js
+++ dogpark/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Jumbotron from "./components/Jumbotron";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <div><h1>Hello World</h1></div>
-       
-      </header>
+      <Nav />
+      <Header />
     </div>
   );
 }
