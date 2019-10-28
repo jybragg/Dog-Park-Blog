a9cc32ab20a2e213a4177c2bbafe12bee267020b
Jasmin <jasymatos@gmail.com>
2019-10-28 15:11:38 -0400

    added sign up, login, and log out

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Nav from "./components/Nav";
-import Header from "./components/Header";
-// import Jumbotron from "./components/Jumbotron";
+// import Header from "./components/Header";
+import Jumbotron from "./components/Jumbotron";
 // import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NotFound from './components/NotFound';
-import Home from './components/Home';
+import Home from './pages/Home';
 import HelloWorld from './components/Blog';
+import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <Nav />
-      <Header />
+      <Jumbotron />
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Login} />
+        <Route path="/login" component={Login} />
         <Route path="/helloworld" component={HelloWorld} />
         <Route component={NotFound} />
       </Switch>
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,17 +7,12 @@ class Header extends Component {
         return (
             // <!-- Page Header -->
             <header class="masthead img-flex">
-              <div class="overlay"></div>
-              <div class="container">
-                <div class="row">
-                  <div class="col-lg-8 col-md-10 mx-auto">
+               
                     <div class="site-heading">
                       <h1>Clean Blog</h1>
                       <span class="subheading">A Blog Theme by Start Bootstrap</span>
                     </div>
-                  </div>
-                </div>
-              </div>
+              
             </header>
         )
     }
